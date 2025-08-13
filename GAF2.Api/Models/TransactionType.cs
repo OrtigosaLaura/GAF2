72fa92a14ff03586637402993c320bcac746a0ef
OrtigosaLaura <lauraortigosa378>
2025-08-13 10:46:45 -0300

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ GAF2.Api/Models/TransactionType.cs
@@ -0,0 +1,8 @@
+namespace GAF2.Api.Models
+{
+    public enum TransactionType
+    {
+        Receita = 1,
+        Despesa = 2
+    }
+}
---
+++ GAF2.Api/Models/User.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace GAF2.Api.Models;
+
+[Table("users")]
+
+    public class User : IdentityUser
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
